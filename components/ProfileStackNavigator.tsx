b33e1adb4844813f2aef3154ee8366cd87faf64f
jugodfroy <julien.godfroy@isep.fr>
2021-03-12 12:02:34 +0100

    implementation profile stack nav

--- components/ProfileStackNavigator.tsx
+++ components/ProfileStackNavigator.tsx
@@ -1,15 +1,197 @@
-import {createStackNavigator} from 'react-navigation-stack'
-import {createAppContainer} from 'react-navigation'
 
-import Profile from './Profile'
+import * as React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import { StyleSheet, View, Text, TouchableOpacity, ScrollView, Image } from 'react-native';
+import { Ionicons, MaterialCommunityIcons, Feather } from '@expo/vector-icons';
 
-const ProfileStackNavigator = createStackNavigator({
-    Profile:{
-        screen : Profile,
-        navigationOptions:{
-            title : "Profile"
-        }
-    }
+
+function ProfileScreen({ navigation }) {
+  return (
+    <ScrollView>
+            <View>
+
+                <View style={style.main_profile_container}>
+                    <View>
+                        <Image source={require("./img/pp.jpg")} style={style.pp} />
+                    </View>
+        
+                    <View style={style.name}>
+                        <Text style={style.text_name}>Julien</Text>
+                        <Text style={[style.text_name, {fontWeight:'bold'}]}>GODFROY</Text>
+                    </View>
+        
+                    <View style={{flexDirection : 'row'}}>
+                        <View style={style.stat_bloc}>
+                            <Text style={style.stat_figures}>10</Text>
+                            <Text style={style.stat_text}>Tokens</Text>
+                        </View>
+                        <View style={style.stat_bloc_center}>
+                            <Text style={style.stat_figures}>4</Text>
+                            <Text style={style.stat_text}>Ventes</Text>
+                        </View>
+                        <View style={style.stat_bloc}>
+                            <Text style={style.stat_figures}>5</Text>
+                            <Text style={style.stat_text}>Achats</Text>
+                        </View>
+                    </View>
+                </View>
+                <View style={style.menu_container}>
+                    <TouchableOpacity onPress={() => navigation.navigate('History')}>
+                        <View style={style.menu_button}>
+                            <MaterialCommunityIcons name="account-details-outline" color={"#E67E22"} size={26} />
+                            <Text style={style.text_menu}>Mes informations</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('History')}>
+                        <View style={style.menu_button}>
+                            <MaterialCommunityIcons name="history" color={"#E67E22"} size={26} />
+                            <Text style={style.text_menu}>Mon historique</Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('History')}>
+                        <View style={style.menu_button}>
+                            <Ionicons name="settings-outline" color={"#E67E22"} size={26} />
+                            <Text style={style.text_menu}>Paramètres de l'application </Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('History')}>
+                        <View style={style.menu_button}>
+                            <Ionicons name="information-circle-outline" color={"#E67E22"} size={26} />
+                            <Text style={style.text_menu}>Informations sur l'application </Text>
+                        </View>
+                    </TouchableOpacity>
+                    <TouchableOpacity onPress={() => navigation.navigate('History')}>
+                        <View style={style.menu_button}>
+                            <Feather name="x-octagon" color={"#E67E22"} size={26} />
+                            <Text style={style.text_menu_red}>Se déconnecter </Text>
+                        </View>
+                    </TouchableOpacity>
+                    
+                </View>
+                
+            </View>
+        </ScrollView>
+  );
+}
+
+
+
+
+const Stack = createStackNavigator();
+
+function App() {
+  return (
+    <NavigationContainer independent={true}>    
+      <Stack.Navigator initialRouteName="ProfileScreen" >
+        <Stack.Screen name="ProfileScreen" component={ProfileScreen} />
+        <Stack.Screen name="EditProfile" component={Todev} />
+        <Stack.Screen name="History" component={Todev} />
+        <Stack.Screen name="Settings" component={Todev} />
+        <Stack.Screen name="Informations" component={Todev} />
+        <Stack.Screen name="Disconnect" component={Todev} />
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+}
+
+function Todev() {
+  return (
+    <View style={{ flex: 1, alignItems: 'center', justifyContent: 'center' }}>
+      <Text>IN PROGRESS</Text>
+    </View>
+  );
+}
+
+
+
+const style=StyleSheet.create({
+  //profile section
+  main_profile_container:{
+      alignSelf : 'center',
+      paddingTop : 40,
+  },
+  
+  pp:{
+      height : 170,
+      width : 170,
+      borderRadius : 50,
+      alignSelf : 'center',
+  },
+
+  name: {
+      paddingTop : 10,
+      alignSelf : 'center',
+      paddingBottom : 10,
+
+  },
+
+  text_name: {
+      alignSelf : 'center',
+      fontSize : 22,
+  },
+  
+  stat_bloc:{
+      paddingHorizontal : 15,
+      marginTop :10,
+  },
+  
+  stat_figures:{
+      alignSelf : 'center',
+      fontWeight : 'bold',
+      fontSize : 25,
+      color : '#E67E22'
+  },
+
+
+  stat_bloc_center:{
+      paddingHorizontal : 10,
+      marginTop :10,
+      borderLeftWidth : 1,
+      borderRightWidth : 1,
+
+  },
+
+  stat_text:{
+      marginHorizontal : 10,
+      color : '#2980B9',
+      fontWeight : 'bold'
+  }, 
+
+
+  //menu section
+
+  menu_container:{
+      paddingHorizontal : 20,
+      marginHorizontal : 10,
+      paddingVertical : 10,
+      backgroundColor : '#fff',
+      marginTop : 30,
+      borderRadius : 50,
+      marginBottom:30,
+  },
+
+  menu_button:{
+      flexDirection : 'row',
+      paddingVertical : 10,
+     
+
+  
+  },
+
+  text_menu:{
+      alignSelf : 'center', 
+      paddingLeft : 10,
+      fontSize : 15
+  },
+
+  text_menu_red:{
+      alignSelf : 'center', 
+      paddingLeft : 10,
+      fontSize : 15,
+      color : '#e74c3c'   //beau rouge pour la deconnexion
+  },
 })
 
-export default ProfileStackNavigator
+
+export default App;
